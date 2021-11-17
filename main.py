82670fc421b1f8a49562416dbeac07ddde2b1a9f
Dawson <dawson.catton@gmail.com>
2021-11-17 10:06:32 -0500

    Got Display Working

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print('hello world')
+import pygame
+from pygame.constants import K_DOWN, K_a
+import pygame.sprite
+
+
+class Board:
+    def __init__(self) -> None:
+    self.running = True
+    (self.width, self.height) = (300, 200)
+    self.screen = pygame.display.set_mode((width, height))
+    
+
+    def run_loop(self):
+        keys=pygame.key.get_pressed()
+        while running:
+            pygame.display.flip()
+
+            for event in pygame.event.get():
+                
+                
+            
+                if event.type == pygame.QUIT:
+                    running = False
---
+++ pieces.py
@@ -0,0 +1,11 @@
+
+
+class Rook:
+    def __init__(self) -> None:
+        pass
+class Bishop:
+    def __init__(self) -> None:
+        pass
+class King: 
+    def __init__(self) -> None:
+        pass   
