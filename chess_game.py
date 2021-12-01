900a3ba59107ec97f876ffce986d0cbf16cef7c7
Dawson <dawson.catton@gmail.com>
2021-12-01 10:18:57 -0500

    got the rook working with basics now working on bishop

--- chess_game.py
+++ chess_game.py
@@ -1,19 +1,21 @@
 
 
 class Rook:
-    def __init__(self,col,row,board) -> None:
+    def __init__(self,col,row,board,color) -> None:
         self.pos=[row,col]
         self.board=board
+        self.color=color
 class Bishop:
-    def __init__(self,col,row,board) -> None:
+    def __init__(self,col,row,board,color) -> None:
         self.pos=row,col
         self.board=board
+        self.color=color
         
 class King: 
-    def __init__(self,col,row,board) -> None:
+    def __init__(self,col,row,board,color) -> None:
         self.pos=row,col
         self.board=board
-
+        self.color=color
 class Board:
     def __init__ (self):
         self.display_board = [ ["."] * 8 for i in range(8)]
@@ -22,7 +24,8 @@ class Board:
         self.y_change=False
         #print(self)
         self.rw=Rook(4,4,self.board)
-        
+        self.wb=Bishop(1,1,self.board)
+        self.display_board[self.wb.pos[0]][self.wb.pos[1]]='B'
         self.display_board[self.rw.pos[0]][self.rw.pos[1]]='R'
         self.board[self.rw.pos[0]][self.rw.pos[1]]=self.rw
     def get_fov(self,piece)->tuple:
@@ -44,42 +47,58 @@ class Board:
         user.lower()
         
         user=user.split(' ')
+        piece=user[0]
         print(user)
-        if user[0]=='forward'or user[0]=='backwards':
-            self.y_change=True
-            if user[0]=='backwards':
-                self.direction=-1
-            else:
-                self.direction=1
-        if user[0]=='right'or user[0]=='left':
-            self.x_change=True
-            self.y_change=False
-            if user[0]=='left':
-                self.direction=1
-            else:
-                self.direction=-1
-        if self.y_change:
-            self.display_board[self.rw.pos[0]][self.rw.pos[1]]="."
-            self.display_board[self.rw.pos[0] - (self.direction * int(user[1]))][self.rw.pos[1]]='R'
-            
-            self.board[self.rw.pos[0]][self.rw.pos[1]]="."
-            self.board[self.rw.pos[0] - (self.direction * int(user[1]))][self.rw.pos[1]]=self.rw
-            self.rw.pos=[(self.rw.pos[0] - (self.direction * int(user[1]))),self.rw.pos[1]]
+        if piece=='rook':
+            print('rook')
+            if user[1]=='forward'or user[1]=='backwards':
+                self.y_change=True
+                if user[1]=='backwards':
+                    self.direction=-1
+                else:
+                    self.direction=1
+            if user[1]=='right'or user[1]=='left':
+                self.x_change=True
+                self.y_change=False
+                if user[1]=='left':
+                    self.direction=1
+                else:
+                    self.direction=-1
+            if self.y_change:
+                self.display_board[self.rw.pos[0]][self.rw.pos[1]]="."
+                self.display_board[self.rw.pos[0] - (self.direction * int(user[2]))][self.rw.pos[1]]='R'
+                
+                self.board[self.rw.pos[0]][self.rw.pos[1]]="."
+                self.board[self.rw.pos[0] - (self.direction * int(user[2]))][self.rw.pos[1]]=self.rw
+                self.rw.pos=[(self.rw.pos[0] - (self.direction * int(user[2]))),self.rw.pos[1]]
 
-            print(self.rw.pos)
-            self.display_update()
-        if self.x_change:
-            self.display_board[self.rw.pos[0]][self.rw.pos[1]]="."
-            self.display_board[self.rw.pos[0]][self.rw.pos[1] - (self.direction * int(user[1]))]='R'
-            
-            self.board[self.rw.pos[0]][self.rw.pos[1]]="."
-            self.board[self.rw.pos[0]][self.rw.pos[1] - (self.direction * int(user[1]))]=self.rw
-            self.rw.pos=[self.rw.pos[0],(self.rw.pos[1] - (self.direction * int(user[1])))]
-            if self.rw.pos[0]<0:
-                print('error')
+                print(self.rw.pos)
+                self.display_update()
+            if self.x_change:
+                self.display_board[self.rw.pos[0]][self.rw.pos[1]]="."
+                self.display_board[self.rw.pos[0]][self.rw.pos[1] - (self.direction * int(user[2]))]='R'
+                
+                self.board[self.rw.pos[0]][self.rw.pos[1]]="."
+                self.board[self.rw.pos[0]][self.rw.pos[1] - (self.direction * int(user[2]))]=self.rw
+                self.rw.pos=[self.rw.pos[0],(self.rw.pos[1] - (self.direction * int(user[2])))]
+                self.display_update()
+                if self.rw.pos[0]<0:
+                    print('error')
             if self.rw.pos[1]<0:
-                print('')
-            self.display_update()
+                print('error')
+                self.display_update()
+        if piece=='bishop':
+            y_negative_change=False
+            x_negative_change=False
+            if user[1]=='down':
+                y_negative_change=True
+            if user[1]=='left':
+                x_negative_change=True
+            
+            if y_negative_change and x_negative_change:
+                
+            
+
    
     def display_update(self):
         foo=0
