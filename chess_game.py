4427242d8b0bf8cc735d2d2a7d93689e6979ba65
Dawson <dawson.catton@gmail.com>
2021-12-08 10:22:41 -0500

    moved the move functions to there piece classes, and got rid of display list so that I dont have two lists

--- chess_game.py
+++ chess_game.py
@@ -1,21 +1,83 @@
 
+class Piece:
+    def __init__(self,row,col,board,color) -> None:
+        self.pos=[row,col]
+        self.board=board
+        self.color=color
+class Rook(Piece):
+    def __init__(self,col,row,board,color) -> None:
+        self.pos=[row,col]
+        self.value=5
+        self.board=board
+        self.color=color
+        self.name='rook'
+        self.letter='R'
+    def move(self,y_change,user,direction,piece,board,display_board):
+        if y_change:
+            display_board[piece.pos[0]][piece.pos[1]]="."
+            display_board[piece.pos[0] - (direction * int(user[2]))][piece.pos[1]]='R'
+            
+            board[piece.pos[0]][piece.pos[1]]="."
+            board[piece.pos[0] - (direction * int(user[2]))][piece.pos[1]]=piece
+            piece.pos=[(piece.pos[0] - (direction * int(user[2]))),piece.pos[1]]
+        else:
+            display_board[piece.pos[0]][piece.pos[1]]="."
+            display_board[piece.pos[0]][piece.pos[1] - (direction * int(user[2]))]='R'
+            board[piece.pos[0]][piece.pos[1]]="."
+            board[piece.pos[0]][piece.pos[1] - (direction * int(user[2]))]=piece
+            piece.pos=[piece.pos[0],(piece.pos[1] - (direction * int(user[2])))]
+        print(display_board)
+        return board
+class Bishop(Piece):
+    def __init__(self,col,row,board,color) -> None:
+        Piece.__init__(self,col,row,board,color)
+        self.name='bishop'
+        self.letter='B'
+        
+    def move(self,y_negative_change,x_negative_change,user,piece,board,display_board) :
+        if y_negative_change and x_negative_change:
+            print('down left')
+            board[piece.pos[0]][piece.pos[1]]='.'
+            board[piece.pos[0]+int(user[3])][piece.pos[1]-int(user[3])]=piece
 
-class Rook:
+            display_board[piece.pos[0]][piece.pos[1]]='.'
+            display_board[piece.pos[0]+int(user[3])][piece.pos[1]-int(user[3])]='B'
+            
+
+        elif y_negative_change and not x_negative_change:
+            print('down right')
+            board[piece.pos[0]][piece.pos[1]]='.'
+            board[piece.pos[0]+int(user[3])][piece.pos[1]+int(user[3])]=piece
+
+            display_board[piece.pos[0]][piece.pos[1]]='.'
+            display_board[piece.pos[0]+int(user[3])][piece.pos[1]+int(user[3])]='B'
+            y_negative_change=False
+            x_negative_change=False
+            
+        elif not y_negative_change and x_negative_change:
+            print('up left')
+            board[piece.pos[0]][piece.pos[1]]='.'
+            board[piece.pos[0]-int(user[3])][piece.pos[1]-int(user[3])]=piece
+
+            display_board[piece.pos[0]][piece.pos[1]]='.'
+            display_board[piece.pos[0]-int(user[3])][piece.pos[1]-int(user[3])]='B'
+            y_negative_change=False
+            x_negative_change=False
+
+        elif not y_negative_change and not x_negative_change:
+            print('up right')
+            board[piece.pos[0]][piece.pos[1]]='.'
+            board[piece.pos[0]-int(user[3])][piece.pos[1]+int(user[3])]=piece
+
+            display_board[piece.pos[0]][piece.pos[1]]='.'
+            display_board[piece.pos[0]-int(user[3])][piece.pos[1]+int(user[3])]='B'
+        return [board,display_board]
+class King(Piece): 
     def __init__(self,col,row,board,color) -> None:
         self.pos=[row,col]
         self.board=board
         self.color=color
-class Bishop:
-    def __init__(self,col,row,board,color) -> None:
-        self.pos=row,col
-        self.board=board
-        self.color=color
-        
-class King: 
-    def __init__(self,col,row,board,color) -> None:
-        self.pos=row,col
-        self.board=board
-        self.color=color
+
 class Board:
     def __init__ (self):
         self.display_board = [ ["."] * 8 for i in range(8)]
@@ -23,13 +85,18 @@ class Board:
         self.x_change=False
         self.y_change=False
         #print(self)
-        self.rw=Rook(4,4,self.board)
-        self.wb=Bishop(1,1,self.board)
+        self.direction=0
+        self.test=False
+        self.rw=Rook(4,4,self.board,'white')
+        self.wb=Bishop(1,1,self.board,'white')
         self.display_board[self.wb.pos[0]][self.wb.pos[1]]='B'
         self.display_board[self.rw.pos[0]][self.rw.pos[1]]='R'
         self.board[self.rw.pos[0]][self.rw.pos[1]]=self.rw
+        self.board[self.wb.pos[0]][self.wb.pos[1]]=self.wb
     def get_fov(self,piece)->tuple:
         pass
+
+
     def game_loop(self):
         running = True
         
@@ -39,6 +106,9 @@ class Board:
             if user_input.lower()=='play':
                 
                 self.make_moves()
+            elif user_input.lower()=='reset display':
+                self.display_update()
+
             if user_input.lower()=='stop':
                 running=False
 
@@ -48,10 +118,12 @@ class Board:
         
         user=user.split(' ')
         piece=user[0]
+        pos=self.rw.pos
         print(user)
         if piece=='rook':
             print('rook')
-            if user[1]=='forward'or user[1]=='backwards':
+            piece=self.rw
+            if user[1]=='up'or user[1]=='down':
                 self.y_change=True
                 if user[1]=='backwards':
                     self.direction=-1
@@ -64,51 +136,71 @@ class Board:
                     self.direction=1
                 else:
                     self.direction=-1
-            if self.y_change:
-                self.display_board[self.rw.pos[0]][self.rw.pos[1]]="."
-                self.display_board[self.rw.pos[0] - (self.direction * int(user[2]))][self.rw.pos[1]]='R'
-                
-                self.board[self.rw.pos[0]][self.rw.pos[1]]="."
-                self.board[self.rw.pos[0] - (self.direction * int(user[2]))][self.rw.pos[1]]=self.rw
-                self.rw.pos=[(self.rw.pos[0] - (self.direction * int(user[2]))),self.rw.pos[1]]
-
-                print(self.rw.pos)
-                self.display_update()
-            if self.x_change:
-                self.display_board[self.rw.pos[0]][self.rw.pos[1]]="."
-                self.display_board[self.rw.pos[0]][self.rw.pos[1] - (self.direction * int(user[2]))]='R'
-                
-                self.board[self.rw.pos[0]][self.rw.pos[1]]="."
-                self.board[self.rw.pos[0]][self.rw.pos[1] - (self.direction * int(user[2]))]=self.rw
-                self.rw.pos=[self.rw.pos[0],(self.rw.pos[1] - (self.direction * int(user[2])))]
-                self.display_update()
-                if self.rw.pos[0]<0:
-                    print('error')
-            if self.rw.pos[1]<0:
+           
+            
+            new_stage=piece.move(self.y_change,user,self.direction,piece,self.board,self.display_board)[0]
+            self.board=new_stage[0]
+            self.display_board=new_stage[1]
+            
+            if piece.pos[0]<0:
                 print('error')
+            elif piece.pos[1]<0:
+                print('error')
+                
+            else:
                 self.display_update()
         if piece=='bishop':
+            piece=self.wb
             y_negative_change=False
             x_negative_change=False
             if user[1]=='down':
                 y_negative_change=True
-            if user[1]=='left':
+            if user[2]=='left':
                 x_negative_change=True
-            
-            if y_negative_change and x_negative_change:
-                
+            print(y_negative_change)
+            print(x_negative_change)
+
             
 
-   
+            if self.wb.pos[0]<0:
+                print('error')
+            elif self.wb.pos[1]<0:
+                print('error')
+            else:
+                self.display_update()
+
     def display_update(self):
-        foo=0
-        print("  A B C D E F G H")
-        for i in self.display_board:
-            foo+=1
-            print(foo,'|'.join(i))
-            
+        #play
+        # print(self.board)
+        for line in self.board:
+            for square in line:
+                if isinstance(square,Piece):
+                    print ('object')
+      
+        print("   A B C D E F G H")
+        for rn,row in enumerate(self.board):
+            print(rn+1,end='')
+            for square in row:
+                
+                if isinstance(square,Piece):
+                    
+                    print (square.letter,end='')
+                    
+                else:
+                    print(square,end='')
+                print('|',end='')
+            print('')
+    def check_for_take(self):
+        pass
+
+
+def test_rook():
+    bored=Board()
+    bored.display_update()
+
     
-    
+
+
 bored=Board()
 bored.display_update()
 bored.game_loop()
